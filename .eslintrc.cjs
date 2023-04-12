2c8dd5ba26bb2ffe6a5f7aa6e906fc615abd32b0
Daryll Clarke <daryll.clarke@deepl.com>
2023-04-12 08:40:31 +0100

    Configure eslint + prettier completely!

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "airbnb", "prettier"],
+  plugins: ["react", "prettier"],
+  rules: {
+    indent: ["error", 2],
+    "spaced-comment": 0,
+    "react/react-in-jsx-scope": 0,
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "all",
+        tabWidth: 2,
+        semi: false,
+        bracketSpacing: true,
+        eslintIntegration: true,
+        printWidth: 120,
+      },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [["@", "./src"]],
+      },
+    },
+  },
+}
--- src/pages/dashboard/music.jsx
+++ src/pages/dashboard/music.jsx
@@ -1,58 +1,47 @@
-import { useEffect, useState } from "react";
-import {
-  accessToken,
-  logout,
-  getCurrentUserProfile,
-} from "@/helpers/spotify.js";
-import { Button } from "@material-tailwind/react";
+import { useEffect, useState } from "react"
+import { Button } from "@material-tailwind/react"
+import { accessToken, logout, getCurrentUserProfile } from "@/helpers/spotify"
 
 export function Music() {
-  const [token, setToken] = useState(null);
-  const [profile, setProfile] = useState(null);
+  const [token, setToken] = useState(null)
+  const [profile, setProfile] = useState(null)
 
   useEffect(() => {
-    setToken(accessToken);
+    setToken(accessToken)
 
     async function fetchData() {
       try {
-        const { data } = await getCurrentUserProfile();
-        setProfile(data);
+        const { data } = await getCurrentUserProfile()
+        setProfile(data)
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
     }
-    
-    fetchData();
-  }, []);
+
+    fetchData()
+  }, [])
 
   return (
-    <>
-      <header>
-        {!token ? (
-          <a
-            className="App-link"
-            href="http://localhost:8888/api/music/spotify/login"
-          >
-                        Log in to Spotify
-          </a>
-        ) : (
-          <>
-            <h1>Music Page</h1>
-            <Button onClick={logout}>Logout</Button>
-            {profile && (
-              <div
-                className={`bg-[url(${profile.images[0].url})]`}
-              >
-                <h3>{profile.display_name}</h3>
-                <p>{profile.product}</p>
-                <p>{profile.followers.total}</p>
-              </div>
-            )}
-          </>
-        )}
-      </header>
-    </>
-  );
+    <header>
+      {!token ? (
+        <a className="App-link" href="http://localhost:8888/api/music/spotify/login">
+          Log in to Spotify
+        </a>
+      ) : (
+        <>
+          <h1>Music Page</h1>
+          <Button onClick={logout}>Logout</Button>
+          {profile && (
+            <div className="mb-4 mt-2 h-20 w-80 rounded p-2">
+              <h3>{profile.display_name}</h3>
+              <p>{profile.product}</p>
+              <p>{profile.followers.total}</p>
+            </div>
+          )}
+        </>
+      )}
+    </header>
+  )
 }
 
-export default Music;
+export default Music
